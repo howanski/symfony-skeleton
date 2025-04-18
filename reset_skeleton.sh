c65d30cebbeb221a0bd6f91d38202cfa7f49e34f
Maciej Howański <howanski@proton.me>
2025-04-18 13:33:00 +0200

    feat(skeleton): automate self-signed certificate
    + bump php version

---
+++ reset_skeleton.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+clear
+rm -f bin-cache/composer.phar
+rm -f bin-cache/symfony
+rm -f caddy/cert.pem
+rm -f caddy/key.pem
+rm -rf app/symfony
+rm -rf ide_data
+rm -rf postgres_data
+rm -rf mysql_data
--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 clear
+source .env
 if [ ! -f "caddy/key.pem" ]; then
-    echo "We need to generate TLS keys first"
-    openssl req -x509 -newkey rsa:4096 -keyout caddy/key.pem -out caddy/cert.pem -days 3650 -nodes
+    echo "We need to generate TLS keys first - creating generic self-signed certificate"
+    openssl req -x509 -newkey rsa:4096 -keyout caddy/key.pem -out caddy/cert.pem -days 3650 -subj "/C=$SSL_CERT_SELF_SIGNED_COUNTRY/ST=$SSL_CERT_SELF_SIGNED_STATE/L=$SSL_CERT_SELF_SIGNED_CITY/CN=$SSL_CERT_SELF_SIGNED_WWW_ADDRESS" -nodes
 fi
 
 mkdir -p postgres_data
